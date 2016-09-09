e102218129d865af46b494459268db29aaab1126
Dasheng Chen <dchen@rollins.edu>
2016-09-09 14:06:03 -0400

    added search

--- js/script.js
+++ js/script.js
@@ -8,6 +8,46 @@ var markers = [
         lng: -81.36188
     },
     {
+        title: "Pizzeria Valdiano",
+        lat: 28.60169,
+        lng: -81.36223
+    },
+    {
+        title: "Publix Super Market at Winter Park Village",
+        lat: 28.60089,
+        lng: -81.36288
+    },
+    {
+        title: "The Cheesecake Factory",
+        lat: 28.60288,
+        lng: -81.36274
+    },
+    {
+        title: "Starbucks",
+        lat: 28.60321,
+        lng: -81.36464
+    },
+    {
+        title: "Game Stop",
+        lat: 28.60225,
+        lng: -81.36337
+    },
+    {
+        title: "BRIO Tuscan Grille",
+        lat: 28.60201,
+        lng: -81.36393
+    },
+    {
+        title: "Guitar Center",
+        lat: 28.60315,
+        lng: -81.36282
+    },
+    {
+        title: "Paseo at Winter Park Village",
+        lat: 28.59972,
+        lng: -81.36184
+    },
+    {
         title: "Ruth's Chris Steak House",
         lat: 28.60361,
         lng: -81.36388
@@ -21,15 +61,23 @@ function initMap() {
 
     // Create a map object and specify the DOM element for display.
     map = new google.maps.Map(document.getElementById('map'),{
-        zoom: 18,
+        zoom: 17,
         center: myLatLng
     })
 
     // Create a marker and set its position.
+    var image = {
+        url: 'images/peacock.png',
+        scaledSize: new google.maps.Size(100, 100),
+        origin: new google.maps.Point(0, 0),
+        // The anchor for this image is the base of the flagpole at (0, 32).
+        anchor: new google.maps.Point(0, 30)
+    };
     var center = new google.maps.Marker({
       map: map,
       position: myLatLng,
-      title: 'Winter Park Village'
+      title: 'Winter Park Village',
+      icon: image
     });
 
     ko.applyBindings(new viewModel());
@@ -55,15 +103,19 @@ function foursquareQuery(lat, lng, name){
 
 var infowindowDefault = '<div style="width: 100px; height: 30px;"><p style="text-align: center;">Loading</p></div>';
 
+// Define viewModel
 var viewModel = function() {
     var self = this;
+    // initiate observable to store user input
+    self.userInput = ko.observable("");
 
+    // initiate oberservable array to store info of locations
     self.locationList = ko.observableArray([]);
-
     markers.forEach(function(marker){
         self.locationList.push(new loc(marker));
     });
 
+    // initiate google map infowindow
     self.infowindow = new google.maps.InfoWindow({
         content: infowindowDefault
     });
@@ -72,11 +124,23 @@ var viewModel = function() {
         self.infowindow.setContent(infowindowDefault);
         self.getFoursquareData(location);
     }
+    // initiate a list of markers to show
+    self.markersList = ko.computed(function(){
+        var list = [];
+        var length = self.locationList().length;
 
+        for(var i = 0; i < length; i++ ){
+            if (self.locationList()[i].title().toLowerCase().indexOf(self.userInput().toLowerCase()) != -1) {
+                self.locationList()[i].showMarker(map);
+            }
+            else {
+                // for the items that are not a match, we turn off the markers
+                self.locationList()[i].showMarker(null);
+            }
+        }
+    });
 
-    /*
-        Download Foursqaure data asynchronously using ajax
-    */
+    // Download Foursqaure data asynchronously using ajax
     self.getFoursquareData = function(venue) {
             var url = foursquareQuery(venue.lat, venue.lng, venue.title());
 
@@ -84,7 +148,10 @@ var viewModel = function() {
                 .done(function(data) {
                     console.log("successfully downloaded foursquare data");
                     var result = data.response.venues[0];
-                    var output = result.name + "<hr>" + "<strong>Address: </strong>" + result.location.formattedAddress;
+                    var category = result.categories[0].name;
+                    var name = result.name;
+                    var address = result.location.formattedAddress;
+                    var output = name + " - " + category + "<hr>" + "<strong>Address: </strong>" + address;
                     self.infowindow.setContent(output);
             })
             .fail(function(){
@@ -94,7 +161,7 @@ var viewModel = function() {
 
     // iterate through locationList ko observable array
     self.locationList().forEach(function(location){
-        // add event listener for 'click' to each location.marker then we pass location to the return function, then run it right after with location as parameter
+        // add event listener for 'click' to each location.marker
         location.marker.addListener('click', (function(){
             return function(){
                 self.infowindowOutput(location);
@@ -106,21 +173,20 @@ var viewModel = function() {
 
 
 
-
+// loc object to store info of markers
 var loc = function(marker){
     var self = this;
     self.title = ko.observable(marker.title);
     self.lat = marker.lat;
     self.lng = marker.lng;
 
-
     self.marker = new google.maps.Marker({
         position: {lat: self.lat, lng: self.lng},
-        map: map
+        map: null
     })
 
     self.showMarker = function(map) {
-        if (map !== null && self.marker.map === null){
+        if (map !== null ){
             self.marker.setMap(map);
         }
         else{
